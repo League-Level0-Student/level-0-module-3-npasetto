16deb22b3d97f5fa29b711b379786c9fd8d72cb7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-24 17:17:13 -0700

    Checkpoint

---
+++ src/Oddly_Loopy.java
@@ -0,0 +1,8 @@
+
+public class Oddly_Loopy {
+public static void main(String[] args) {
+	for (int i = 0; i < 250; i++) {
+		System.out.println(1+2*i);
+	}
+}
+}
---
+++ src/SpudCounter.java
@@ -0,0 +1,13 @@
+
+public class SpudCounter {
+public static void main(String[] args) {
+	for (int i = 0; i < 7; i++) {
+		if(i==3) {
+		System.out.println(i+1);	
+		}else {
+		System.out.print((i+1)+" potato, ");
+		}
+	}
+	System.out.println("more");
+}
+}
